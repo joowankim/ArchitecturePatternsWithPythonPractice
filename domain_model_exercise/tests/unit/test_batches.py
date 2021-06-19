22a4edaf93a2a036f1a19be2c3563d746b030bbd
Joowan Kim <rlawndhks217@gmail.com>
2021-06-19 16:12:40 +0900

    Add deallocate

--- domain_model_exercise/tests/unit/test_batches.py
+++ domain_model_exercise/tests/unit/test_batches.py
@@ -38,3 +38,9 @@ def test_cannot_allocate_if_skus_do_not_match():
     batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
     different_sky_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
     assert batch.can_allocate(different_sky_line) is False
+
+
+def test_can_only_deallocate_allocated_line():
+    batch, unallocated_line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 20
